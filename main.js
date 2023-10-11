20db4a8310e2430fd3b39d3e5256893f34b362e0
HYEIN-SEP <hyeinpark22@gmail.com>
2023-10-11 18:13:36 +0900

    [FIX]헤더 수정, [ADD]탭 버튼 이미지추가

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-$('.menu_box').click(function () {
-  $('.drop_menu').addClass('active');
+let burger = $('.menu-trigger');
+
+burger.each(function (index) {
+  let $this = $(this);
+
+  $this.on('click', function (e) {
+    e.preventDefault();
+    $(this).toggleClass('active-' + (index + 1));
+    $('nav').toggleClass('active');
+  })
 })

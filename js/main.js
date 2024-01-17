0ebecb072543fad0f8a0ef8e09802d5105126f38
HYEIN-SEP <hyeinpark22@gmail.com>
2024-01-18 03:06:33 +0900

    [FIX]FOOTER css수정

--- js/main.js
+++ js/main.js
@@ -37,10 +37,10 @@ next.click(function () {
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 10,
-  // pagination: {
-  //   el: ".swiper-pagination",
-  //   clickable: true,
-  // },
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
   autoHeight : true,
   navigation: {
     nextEl: '.swiper-button-next',
--- js/portfolio.js
+++ js/portfolio.js
@@ -1,44 +1,64 @@
+let burger = $('.menu-trigger');
+
+burger.each(function (index) {
+  let $this = $(this);
+
+  $this.on('click', function (e) {
+    e.preventDefault();
+    $(this).toggleClass('active-' + (index + 1));
+    $('.menu').toggleClass('active');
+  })
+})
+
+
 $.getJSON('./data.json',
 function(data){
     var html = [];
-    var icons = [];
 
     $.each(data, function(i, item){
         console.log(item);
-            html.push('<div class="contentWrap">');
-            html.push('<div class="content">');
-            html.push('<p class="green">' + item.id + 'project</p>');
-            html.push('<h4>'+ item.title +'</h4>');
-            html.push('<p class="green">프로젝트 목표</p>');
-            html.push('<p>'+ item.purpose +'</p>');
-            html.push('<p class="green">제작기간</p>');
-            html.push('<p>' + item.date +'</p>');
-            html.push('<p class="green">담당파트</p>');
-            html.push('<p>' + item.part + '</p>');
-            html.push('<p class="green">참여율</p>');
-            html.push('<p>'+ item.rate +'</p>');
-            html.push('<p class="green">사용언어 및 프로그램</p>');
+        html.push('<div class="contentWrap" id= ct_'+item.id+'>');
+        html.push('<div class="content">');
+        html.push('<p class="green">' + item.order + ' </p>');
+        html.push('<h4>'+ item.title +'</h4>');
+        html.push('<p class="green">프로젝트 목표</p>');
+        html.push('<p>'+ item.purpose +'</p>');
+        html.push('<p class="green">제작기간</p>');
+        html.push('<p>' + item.date +'</p>');
+        html.push('<p class="green">담당파트</p>');
+        html.push('<p>' + item.part + '</p>');
+        html.push('<p class="green">참여율</p>');
+        html.push('<p>'+ item.rate +'</p>');
+        html.push('<p class="green">사용언어 및 프로그램</p>');
 
-                $.each(data, function(i, item){
-                    html.push('<div class="icon">');
-                    html.push('<div>');
-                    html.push('<img src=' + item.icon.url + 'alt="아이콘">');
-                    html.push('<p>'+ item.icon.name +'</p>');
-                    html.push('</div>');
-                    html.push('</div>');
-                });
-
+        html.push('<div class="icon">');
+        $.each(item.icon, function(i, ic){
+            html.push('<div>');
+            html.push('<img src=' + ic.url + ' alt="아이콘">');
+            html.push('<p>'+ ic.name +'</p>');
             html.push('</div>');
-            html.push('<div class="monitor">');
-            html.push('<img src='+ item.img +' alt="사이트이미지">')
-            html.push('<a class="btn" href="https://hyein-sep.github.io/riotgames_renewal/" target="_blank">')
-            html.push('<span>사이트 보러가기</span>')
-            html.push('</a>')
-            html.push('</div>')
-            html.push('</div>')
-            html.push('</div>')
+        });
+        html.push('</div>');
+
+        html.push('</div>');
+        html.push('<div class="monitor">');
+        html.push('<img src='+ item.img +' alt="사이트이미지">');
+        html.push('<a class="btn" href=' + item.site + ' target="blank">');
+        html.push('<span>사이트 보러가기</span>');
+        html.push('</a>');
+        html.push('</div>');
+        html.push('</div>');
+        html.push('</div>');
     });
     $(".portfolioContents").html(html.join(''));
-    // $(".icon").html(icons.join(''));
-});
 
+    $('.contentWrap').hide();
+    $('.contentWrap:first-child').show();
+    $(".portfolioList li").click(function(){
+      $(this).addClass('active');
+      $(this).siblings().removeClass('active');
+      let idx = $(this).index() + 1
+      $('#ct_' + idx).show();
+      $('#ct_' + idx).siblings().hide();
+    })
+});

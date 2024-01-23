cd9145c5d83a32fb677a4b211c0bf2e9f2e3de48
HYEIN-SEP <hyeinpark22@gmail.com>
2024-01-23 10:33:13 +0900

    [FIX]파이어베이스 연동, 슬라이드 모달 추가

--- js/main.js
+++ js/main.js
@@ -40,6 +40,7 @@ var swiper = new Swiper(".mySwiper", {
   loop: true,
   autoplay: {
     delay: 3000,
+    disableOnInteraction: false 
   },
   autoHeight : true,
   navigation: {
@@ -48,28 +49,47 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
-$('.dataBtn').click(function(){
-  let name = $('#name');
-  let email = $('#email');
-  let msg = $('#msg');
 
-  const db = firebase.database();
-  var postListRef = firebase.database().ref('contact');
-  var newPostRef = postListRef.push();
-
-  function newPostRef(name, email, msg) {
-    firebase.database().ref().set({
-      name: name,
-      email: email,
-      msg : msg
-    });
+$("#inputForm").validate({
+  rules: {
+    name : {
+      required : true,
+    },
+    email : {
+      required : true,
+      email: true
+    },
+    msg : {
+      required : true,
+    }
+  },
+  messages: {
+    name: {
+        required: "이름을 입력해주세요.",
+    },
+    email: {
+        required: "이메일을 입력해주세요",
+        email: "abc@abc.com형식으로 입력해주세요."
+    },
+    msg: {
+      required: "내용을 입력해주세요",
+    }
+  },
+  submitHandler: function(){
+    console.log("submit!");
   }
-  // newPostRef.set({
-  //   name: name,
-  //   email: email,
-  //   msg : msg
-  // });
-})
+});
 
+const modal = document.querySelector('#m1');
+const btnOpenModal=document.querySelector('.btn-open-modal');
 
+btnOpenModal.addEventListener("click", ()=>{
+    modal.style.display="block";
+});
 
+const modal1 = document.querySelector('#m2');
+const btnOpenModal1=document.querySelector('.btn-open-modal1');
+
+btnOpenModal1.addEventListener("click", ()=>{
+    modal1.style.display="block";
+});
